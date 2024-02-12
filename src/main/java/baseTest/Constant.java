2612c8bbb0015b503978f2fd5b1db33f4a2540cb
никита тараканов <ntarak2002@gmail.com>
2024-02-12 20:48:27 +0300

    TestPR

--- src/main/java/baseTest/Constant.java
+++ src/main/java/baseTest/Constant.java
@@ -2,4 +2,5 @@ package baseTest;
 
 public class Constant {
     public final static String base_Url = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login";
+    private final static String test = "Test";
 }
